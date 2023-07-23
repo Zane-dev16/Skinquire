11d339885bb80b4de7e422c1343bf1d0f77fe15d
Irell Zane <irellzanedev@gmail.com>
2023-07-23 17:15:31 +0100

    Update strapi and handle access token

--- frontend/src/app/connect/google/redirect/page.tsx
+++ frontend/src/app/connect/google/redirect/page.tsx
@@ -1,5 +1,41 @@
-const UserProfile: React.FC = () => {
-  return <div>success</div>;
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
+const ConnectProviderRedirect = () => {
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const access_token = searchParams.get("access_token");
+    console.log(access_token);
+
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+    console.log(backendUrl);
+
+    // Check if the access_token and provider are present
+    if (access_token) {
+      // Make a POST request to the backend with the access_token as a query parameter
+      fetch(
+        `${backendUrl}/api/auth/google/callback?access_token=${access_token}`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Handle the response data from the backend
+          console.log(data); // This will contain the "jwt" and "user" information
+        })
+        .catch((error) => {
+          // Handle any errors that occurred during the API request
+          console.error("Error connecting to the backend:", error);
+        });
+    }
+  }, []);
+
+  return <div>Redirecting..</div>;
 };
 
-export default UserProfile;
+export default ConnectProviderRedirect;
