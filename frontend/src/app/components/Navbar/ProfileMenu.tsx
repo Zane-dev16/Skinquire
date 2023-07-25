f1d20440b94b17f64c1efab4061b6ecb79f2f943
Irell Zane <irellzanedev@gmail.com>
2023-07-25 16:23:14 +0100

    Fix z index issue of the profile menu

--- frontend/src/app/components/Navbar/ProfileMenu.tsx
+++ frontend/src/app/components/Navbar/ProfileMenu.tsx
@@ -1,18 +1,52 @@
+"use client";
+import { useState } from "react";
+
 import styles from "./ProfileMenu.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
-export default async function ProfileMenu() {
+export default function ProfileMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <div>
-        <Image
-          src="/profile-user.svg"
-          alt="profile-avatar"
-          width={30}
-          height={30}
-        ></Image>
-        <div>Profile section</div>
-      </div>
+      <Image
+        className={`${styles.profileAvatar} ${isOpen ? styles.open : ""}`}
+        onClick={toggleMenu}
+        src="/profile-user.svg"
+        alt="profile-avatar"
+        width={30}
+        height={30}
+      ></Image>
+      {isOpen && (
+        <div className={styles.profileMenu}>
+          <form>
+            <input type="text" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button type="submit">LOG IN</button>
+          </form>
+          <a href="http://127.0.0.1:1337/api/connect/google">
+            <div className={styles.googleSignIn}>
+              <Image src="/google-icon.svg" alt="" width={20} height={20} />
+              <span className={styles.googleText}>Continue with Google</span>
+            </div>
+          </a>
+
+          <div className={styles.signUp}>
+            <span className={styles.newHereText}>New Here?</span>
+            <Link
+              className={styles.signUpLink}
+              onClick={toggleMenu}
+              href="/register"
+            >
+              Sign Up!
+            </Link>
+          </div>
+        </div>
+      )}
     </>
   );
 }
