53fffb5e3b28c82272c534ec0563c5cef2ba43cc
Irell Zane <irellzanedev@gmail.com>
2023-07-26 10:46:26 +0100

    Added logout on profile menu
    Logout functionality appears on the profile menu when user is logged in

--- frontend/src/app/components/Navbar/ProfileMenu.tsx
+++ frontend/src/app/components/Navbar/ProfileMenu.tsx
@@ -32,6 +32,7 @@ export default function ProfileMenu() {
 
   const router = useRouter();
 
+  const isAuthenticated = !!Cookies.get("token");
   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const { email, password } = formData;
@@ -43,12 +44,16 @@ export default function ProfileMenu() {
       if (data?.login.user) {
         Cookies.set("token", data.login.jwt);
         router.push("/");
+        router.refresh();
       }
-    } catch (error: ApolloError | any) {
-      // Explicitly specify the type as ApolloError
-      console.error("Login Error:", error.message);
-    }
+    } catch (error: ApolloError | any) {}
   };
+
+  function handleLogout() {
+    Cookies.remove("token");
+    router.refresh();
+    router.push("/");
+  }
 
   if (loading) return <div>Login Loading...</div>;
 
@@ -67,48 +72,60 @@ export default function ProfileMenu() {
       ></Image>
       {isOpen && (
         <div className={styles.profileMenu}>
-          <form onSubmit={handleLogin}>
-            <input
-              id="email"
-              className="appearance-none block w-full p-3 leading-5 text-gray-900 border border-gray-200 rounded-lg shadow-md placeholder-text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-            />
-            <input
-              id="password"
-              className="appearance-none block w-full p-3 leading-5 text-gray-900 border border-gray-200 rounded-lg shadow-md placeholder-text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={(e) =>
-                setFormData({ ...formData, password: e.target.value })
-              }
-            />
-            <button type="submit">LOG IN</button>
-          </form>
-          <a href="http://127.0.0.1:1337/api/connect/google">
-            <div className={styles.googleSignIn}>
-              <Image src="/google-icon.svg" alt="" width={20} height={20} />
-              <span className={styles.googleText}>Continue with Google</span>
-            </div>
-          </a>
+          {!isAuthenticated && (
+            <>
+              <form onSubmit={handleLogin}>
+                <input
+                  id="email"
+                  className="appearance-none block w-full p-3 leading-5 text-gray-900 border border-gray-200 rounded-lg shadow-md placeholder-text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+                <input
+                  id="password"
+                  className="appearance-none block w-full p-3 leading-5 text-gray-900 border border-gray-200 rounded-lg shadow-md placeholder-text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <button type="submit">LOG IN</button>
+              </form>
+              <a href="http://127.0.0.1:1337/api/connect/google">
+                <div className={styles.googleSignIn}>
+                  <Image src="/google-icon.svg" alt="" width={20} height={20} />
+                  <span className={styles.googleText}>
+                    Continue with Google
+                  </span>
+                </div>
+              </a>
+              <div className={styles.signUp}>
+                <span className={styles.newHereText}>New Here?</span>
+                <Link
+                  className={styles.signUpLink}
+                  onClick={toggleMenu}
+                  href="/register"
+                >
+                  Sign Up!
+                </Link>
+              </div>
+            </>
+          )}
 
-          <div className={styles.signUp}>
-            <span className={styles.newHereText}>New Here?</span>
-            <Link
-              className={styles.signUpLink}
-              onClick={toggleMenu}
-              href="/register"
-            >
-              Sign Up!
-            </Link>
-          </div>
+          {isAuthenticated && (
+            <div className={styles.logout}>
+              <button onClick={handleLogout}>LOG OUT</button>
+            </div>
+          )}
+
           {error && <div className={styles.error}>Error: {error.message}</div>}
         </div>
       )}
