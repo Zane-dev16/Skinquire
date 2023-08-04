925c8aca615cf7423c60ace37621d2374d786210
Irell Zane <irellzanedev@gmail.com>
2023-08-04 10:12:10 +0100

    Update rating.js
    Disable creating ratings with the rest api

--- backend/src/api/rating/routes/rating.js
+++ backend/src/api/rating/routes/rating.js
@@ -6,4 +6,7 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::rating.rating");
+module.exports = createCoreRouter("api::rating.rating", {
+  except: ["create"],
+  config: {},
+});
