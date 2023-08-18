568de880c39a883012c98dca8197bda169f363a0
Irell Zane <irellzanedev@gmail.com>
2023-08-18 09:50:47 +0100

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,11 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "localhost"),
-  port: env.int("PORT", 1337),
-  url: env("APP_URL", "http://localhost:1337"),
+  proxy: true,
+  url: env("APP_URL"), // Sets the public URL of the application.
   app: {
     keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
