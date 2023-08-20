1e697f2753ffe19142bd5cdfa8afa9774015ae72
Irell Zane <irellzanedev@gmail.com>
2023-08-20 16:44:03 +0100

    Create new middleware.js file production

---
+++ backend/config/env/production/middlewares.js
@@ -0,0 +1,43 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true, // Keep this if applicable
+        directives: {
+          "connect-src": ["'self'", "https:"], // Allow connections to the same origin and HTTPS
+          "img-src": ["'self'", "data:"],
+          "media-src": ["'self'", "data:"],
+          "frame-src": "'self'", // Allow framing from the same origin
+        },
+      },
+      xssFilter: true, // Enable XSS protection
+      hsts: {
+        maxAge: 31536000, // Set HSTS max age to one year
+        includeSubDomains: true, // Apply HSTS to all subdomains
+      },
+      frameguard: {
+        action: "sameorigin", // Set X-Frame-Options to sameorigin
+      },
+    },
+  },
+  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [process.env.APP_URL, process.env.STRAPI_PUBLIC_FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      keepHeaderOnError: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
