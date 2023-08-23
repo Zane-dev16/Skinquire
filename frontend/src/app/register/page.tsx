244e4ee7c91ab06d30a232c5e2012f7a4e6c1f3b
Irell Zane <irellzanedev@gmail.com>
2023-08-23 18:03:59 +0100

    Clean up registration form

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -4,12 +4,10 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
 import Cookies from "js-cookie";
-
-import { SetStateAction } from "react";
 import { userExists, getPasswordError } from "@/utils/usersAndPermissionsUtils";
 import RegisterForm from "../components/Form/RegisterForm";
 import styles from "./page.module.css";
-import { Passero_One } from "next/font/google";
+import { asyncFetcher } from "@/utils/graphql";
 
 interface ResponseData {
   register: {
@@ -44,46 +42,24 @@ export default function RegisterRoute() {
     } else if (passwordError) {
       setError(passwordError);
     } else {
-      const query = `mutation {
-      register(
-        input: { username: "${username}", email: "${email}", password: "${password}" }
-      ) {
-        jwt
-        user {
-          username
-          email
-        }
-      }
-    }`;
       try {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/graphql`,
-          {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({
-              query,
-            }),
+        const data: ResponseData = await asyncFetcher(`mutation {
+          register(
+            input: { username: "${username}", email: "${email}", password: "${password}" }
+          ) {
+            jwt
+            user {
+              username
+              email
+            }
           }
-        );
-        const jsonRes = await res.json();
-        if (res.ok && jsonRes?.data != null) {
-          Cookies.set("token", jsonRes.data.register.jwt, {
-            expires: 7, // Expires in 7 days
-            secure: true,
-            sameSite: "strict",
-          });
-          router.push("/product-list");
-        } else {
-          const errorMessage = jsonRes.errors[0].message;
-          if (errorMessage == "Email or Username are already taken") {
-            setError("Email Already taken");
-          } else {
-            setError(errorMessage || "Registration Failed");
-          }
-        }
+        }`);
+        Cookies.set("token", data.register.jwt, {
+          expires: 7, // Expires in 7 days
+          secure: true,
+          sameSite: "strict",
+        });
+        router.push("/product-list");
       } catch (error) {
         setError("Registration failed"); // Set error state on catch
         console.error("Error:", error);
@@ -93,15 +69,15 @@ export default function RegisterRoute() {
 
   return (
     <main className={styles.signUpPage}>
-      <motion.div layout className={styles.signUpFormContainer}>
-        <motion.div>
+      <div className={styles.signUpFormContainer}>
+        <div>
           <RegisterForm
             callback={registerUser}
             closeForm={() => {}}
             error={error}
           />
-        </motion.div>
-      </motion.div>
+        </div>
+      </div>
     </main>
   );
 }
