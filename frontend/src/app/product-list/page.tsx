c86a1a579519b1e3ba579f5650a9dece3e218806
Irell Zane <irellzanedev@gmail.com>
2023-08-24 10:19:33 +0100

    Update wrong location of metadata

--- frontend/src/app/product-list/page.tsx
+++ frontend/src/app/product-list/page.tsx
@@ -2,6 +2,13 @@ import FilterOptions from "./components/FilterOptions";
 import ProductList from "./components/ProductList";
 import styles from "./page.module.css";
 
+export const metadata: Metadata = {
+  title:
+    "Explore Top-Rated Skincare Products - Search, Rate and Review | Skinquire",
+  description:
+    "Discover a wide range of skincare products on Skinquire's product list. Our platform offers advanced search filters, sorting options, and ratings analysis, empowering you to make informed choices.",
+};
+
 export default async function Page() {
   return (
     <main>
