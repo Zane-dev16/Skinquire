8f03cfb1222bac7af605a6abcad320548d8b5db0
Irell Zane <irellzanedev@gmail.com>
2023-08-26 11:15:18 +0100

    FIx missing Script id

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -47,12 +47,9 @@ export default function RootLayout({
           gtag('config', 'G-GPJSCM6C85');
         `}
           </Script>
-          <Script>
+          <Script id="impact-radius">
             {`
-        (function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){
-          (c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];
-          t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A4734627-80d5-4500-881a-8b588c513ce91.js',
-          'script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');
+(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A4734627-80d5-4500-881a-8b588c513ce91.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');
         `}
           </Script>
           <Navbar></Navbar>
